4cb5171ca5866c798d71cdd92818d1f5352b02d7
DOS SANTOS <danielitto91@gmail.com>
2021-03-10 04:06:10 +0100

    Update apps_server.sh

--- apps_server.sh
+++ apps_server.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+sudo apt-get update
 
-
-apt-get install htop -y
+apt-get install nmap -y
 apt-get install tree -y
 apt-get install screen -y
 apt-get install vim -y
-apt-get install nmap -y
 apt-get install ncdu -y
+apt-get install htop -y
+apt-get install git -y
+apt-get install figlet -y
+apt-get install curl -y
+apt-get install trash-cli -y
+apt-get install openssh-server -y
